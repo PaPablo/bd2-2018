d7bf7463fd8d2843fef6d5d1b728cee76d4fbfe8
Pablo Toledo <pabloatm980@gmail.com>
2018-05-06 23:46:57 -0300

    Add airport creation

--- practica/practica3/laboratorio/scripts/1.sql
+++ practica/practica3/laboratorio/scripts/1.sql
@@ -66,11 +66,55 @@ begin
 end;
 $$ language plpgsql;
 
+create or replace function aeropuerto_unico(varchar) returns boolean as
+$$
+begin
+	return (select $1 not in (select nombre from aeropuerto));
+end
+$$ language plpgsql;
+drop table aeropuerto;
 create table aeropuerto of taeropuerto (
+	primary key(oid),
+	constraint aeropuerto_nombre_check check(aeropuerto_unico(nombre)),
 	constraint aerolineas_check check(check_aerolineas(aerolineas))
+) with oids;
+
+-- function para insertar un aeropuerto
+
+create or replace function insertar_aeropuerto(
+	nombre varchar(90),
+	ubicacion tubicacionAeorpuerto,
+	medidas tmedidaspista,
+	aero_nombres varchar[]
+) returns integer as
+$$
+declare
+	aero_oids oid[];
+	I integer;
+	aeropuerto_creado integer = 0;
+begin
+	for I in 1..array_length(aero_nombres,1) loop
+		aero_oids[I] := get_aerolinea(aero_nombres[I]);
+		raise notice '%', aero_oids[I];
+	end loop;
+	
+	insert into aeropuerto values (
+		nombre,
+		ubicacion,
+		medidas,
+		aero_oids
+	) returning oid into aeropuerto_creado;
+	
+	return aeropuerto_creado;
+end;
+$$ language plpgsql;
+
+select insertar_aeropuerto(
+	'Aeroparque',
+	('CABA','Buenos Aires','Argentina'),
+	(90,120,'Concreto'),
+	array['lan','aerolineas argentinas','turkish']
 );
-
-
 
 -- Insercion de un aeropuerto
 insert into aeropuerto values(
@@ -79,7 +123,6 @@ insert into aeropuerto values(
 	(90,120,'Concreto'),
 	'{"16604","16621"}'
 );
-
 
 -- function para insertar aeropuerto que devuelve su oid
 create or replace function insertar_aerolinea(varchar(100)) returns integer as
